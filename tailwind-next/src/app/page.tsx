759d1e680ad0b8f5e96379132f67e38e00e611c9
thayna <arraisthayna2106@gmail.com>
2024-02-17 22:12:28 -0300

    dark mode with tailwind

--- tailwind-next/src/app/page.tsx
+++ tailwind-next/src/app/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello Tailwind</h1>
+    <div class="container mx-auto p-4">
+    <h1 class="text-2xl font-bold mb-4 dark:text-gray-200">Dark Mode with Tailwind</h1>
+    <p class="mt-4 dark:text-gray-300">
+      No modo claro, o texto é preto e o fundo é branco. No modo escuro, o texto é branco e o fundo é cinza escuro.
+    </p>
+  </div>
   );
 }
