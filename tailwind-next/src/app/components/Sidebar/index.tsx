2001244d67f66379a321b8eb8da32c08ad380385
thayna-bezerra <arraisthayna2106@gmail.com>
2024-02-19 15:19:35 -0300

    used space widget

--- tailwind-next/src/app/components/Sidebar/index.tsx
+++ tailwind-next/src/app/components/Sidebar/index.tsx
@@ -1,18 +1,35 @@
-import { Search } from 'lucide-react'
+import { BarChart, CheckSquare, Cog, Flag, Home, LifeBuoy, Search, SquareStack, Users } from 'lucide-react'
 import { Logo } from "./Logo";
-import { MainNavigation } from './MainNavigation';
+import { NavItem } from './NavItem';
+import { UsedSpaceWidget } from './UsedSpaceWidget';
 
 export function Sidebar()
 {
     return (
-        <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
+        <aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6">
             <Logo />
             <div className="mx-1 flex w-full items-center gap-2 rounded-lg border-zinc-300 px-3 py-2 shadow-sm">
                 <Search className="h-5 w-5 text-zinc-500" />
                 <input className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" placeholder="Search" />
             </div>
 
-            <MainNavigation />
+            <nav className="space-y-0.5">
+                <NavItem title="Home" icon={Home} />
+                <NavItem title="Dashboard" icon={BarChart} />
+                <NavItem title="Projects" icon={SquareStack} />
+                <NavItem title="Tasks" icon={CheckSquare} />
+                <NavItem title="Reporting" icon={Flag} />
+                <NavItem title="Users" icon={Users} />
+            </nav>
+
+            <div className="mt-auto flex flex-col gap-6">
+                <nav className="space-y-0.5">
+                    <NavItem title="Support" icon={LifeBuoy} />
+                    <NavItem title="Support" icon={Cog} />
+                </nav>
+
+                <UsedSpaceWidget />
+            </div>
         </aside>
     )
 }
